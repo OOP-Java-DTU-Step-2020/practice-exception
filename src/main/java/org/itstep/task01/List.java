398f5f3e288d7bfd4b7ffcceed1e87f932baa293
Max Shaptala <maxshaptala@gmail.com>
2020-11-10 16:27:22 +0200

    Init repository

---
+++ src/main/java/org/itstep/task01/List.java
@@ -0,0 +1,21 @@
+package org.itstep.task01;
+
+public class List {
+    private Object[] obj;
+    private int size = -1;
+    private int cur = -1;
+
+    public List(int size) {
+        cur = 0;
+        this.size = size;
+        obj = new Object[size];
+    }
+
+    public void add(Object item) {
+        obj[cur++] = item;
+    }
+
+    public void removeLast() {
+        obj[--cur] = null;
+    }
+}
